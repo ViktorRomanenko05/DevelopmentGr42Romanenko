00078c0a7d9da01a75e34b936340305a90390d06
Viktor <1180505n@gmail.com>
2025-03-13 10:18:16 +0100

    project updated

--- src/main/java/de/ait/javalessons/repositories/CarRepository.java
+++ src/main/java/de/ait/javalessons/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package de.ait.javalessons.repositories;
 
 import de.ait.javalessons.model.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends CrudRepository<Car, String> {
 }
